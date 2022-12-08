c850887403899ed7b069a575895ed95991b80b0a
Maria Gornashevich <lightbrown9874@gmail.com>
2022-12-09 01:40:09 +0300

    fix: change logic for queue

--- src/queue.js
+++ src/queue.js
@@ -1,18 +1,10 @@
-const { NotImplementedError } = require("../extensions/index.js");
+class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
+  }
+}
 
-// const { ListNode } = require('../extensions/list-node.js');
-
-/**
- * Implement the Queue with a given interface via linked list (use ListNode extension above).
- *
- * @example
- * const queue = new Queue();
- *
- * queue.enqueue(1); // adds the element to the queue
- * queue.enqueue(3); // adds the element to the queue
- * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
- * queue.getUnderlyingList() // returns { value: 3, next: null }
- */
 class Queue {
   constructor() {
     this.head = null;
@@ -24,14 +16,23 @@ class Queue {
     return this.head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+  enqueue(value) {
+    const node = new ListNode(value);
+
+    this.head
+      ? (this.tail.next = this.tail = node)
+      : (this.head = this.tail = node);
+
+    this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
+    const current = this.head;
+    this.head = this.head.next;
+    
+    this.length--;
+
+    return current.value;
   }
 }
 
